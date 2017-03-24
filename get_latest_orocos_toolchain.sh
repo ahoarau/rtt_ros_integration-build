ab93c2fd988e2e756d70dec7cac2a250fbb7cd2e
Antoine Hoarau <hoarau.robotics@gmail.com>
2017-03-24 12:06:53 +0100

    Update get_latest_orocos_toolchain.sh

--- get_latest_orocos_toolchain.sh
+++ get_latest_orocos_toolchain.sh
@@ -4,8 +4,11 @@ latest_tag=$(curl -s https://api.github.com/repos/ahoarau/orocos_toolchain-build
 
 echo "Latest tag : $latest_tag"
 
-lastest_release_url=$(curl -s https://api.github.com/repos/ahoarau/orocos_toolchain-build/releases | grep $latest_tag | grep browser_download_url | cut -d '"' -f 4 | grep $ROS_DISTRO)
+cd $HOME
 
+latest_release_url=$(curl -s https://api.github.com/repos/ahoarau/orocos_toolchain-build/releases | grep $latest_tag | grep browser_download_url | cut -d '"' -f 4 | grep $ROS_DISTRO)
+
+curl $latest_release_url | tar xvz
 
 echo "Latest URL : $latest_release_url"
 
