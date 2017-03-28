bf2da7a85520403c32a3b439d931c31827025d93
Antoine Hoarau <hoarau.robotics@gmail.com>
2017-03-28 13:46:35 +0200

    Update get_latest_orocos_toolchain.sh

--- get_latest_orocos_toolchain.sh
+++ get_latest_orocos_toolchain.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-latest_tag=$(curl -s https://api.github.com/repos/ahoarau/orocos_toolchain-build/release | grep tag_name | head -n 1 | cut -d '"' -f 4)
+latest_tag=$(curl -s https://api.github.com/repos/ahoarau/orocos_toolchain-build/releases | grep tag_name | head -n 1 | cut -d '"' -f 4)
 
 echo "Latest tag : $latest_tag"
 
